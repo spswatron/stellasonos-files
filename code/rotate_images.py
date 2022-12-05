37c3cbf892f51e990777215b853dfdda7479fb8f
spswatron <spswatron@gmail.com>
2022-12-05 11:53:48 -0500

    with rotated images

---
+++ code/rotate_images.py
@@ -0,0 +1,24 @@
+import os
+import pathlib
+from PIL import Image
+ 
+def rotate_images(directory, rotated_directory):
+    if not os.path.exists(rotated_directory):
+        os.makedirs(rotated_directory) 
+    for filename in os.listdir(directory):
+        path = os.path.join(directory, filename)
+        # checking if it is a file
+        if os.path.isfile(path):
+            try:
+                original_image = Image.open(path)
+                rotated_image = original_image.rotate(-90)
+                rotated_path = os.path.join(rotated_directory, filename)
+                rotated_image.save(rotated_path)
+                print("saved " + rotated_path + "!")
+            except:
+                continue
+
+dirname = pathlib.Path(__file__).parent.resolve()
+source_dir = os.path.join(dirname, '../images/test-images')
+rotated_dir = os.path.join(dirname, '../rotated_images/test-images')
+rotate_images(source_dir, rotated_dir)
